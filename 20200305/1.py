74a3531e96c9a46acba087c81cdfcb0baffbea55
imBleShi <imbleshi@gmail.com>
2020-03-06 00:19:25 +0800

    1.环境安装与基础知识20200305

---
+++ 20200305/1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+    
+temp_str = input("请输入带符号的温度值，如'23c'，'34f'")
+if temp_str[-1] in ['F','f']:
+    temp_Cels = (eval(temp_str[0:-1]) -32) / 1.8
+    print("转换后的温度是 %.2f摄氏度" % temp_Cels)
+elif temp_str[-1] in ['C','c']:
+    temp_Farhr = 1.8 * eval(temp_str[0:-1]) + 32
+    print("转换后的温度是{:.2f}华氏度".format(temp_Farhr))
+else:
+    print("格式错误")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonLearning-CollegeCourse
-A basic college course.
+
+害，就当圆自己一个也能在 Github 上发点东西的梦想了hhhhh
+
+本人非 计算机本科 专业，不过恰好今年我们通识课也有开设关于 Python 学习的课程，所以还是来写写。
+
+因为是初学者，且非科班专业，如有不正之处还请大佬们包含。
+
+折腾的过程都丢在博客里啦：https://www.bleshi.com/archives/20200305/PythonLearning.html
